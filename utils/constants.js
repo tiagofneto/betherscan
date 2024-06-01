4c5c31b35964d4d7c44e61d2e9667c4b8e777c6c
tiagofneto <tiagofneto29@gmail.com>
2024-06-01 11:18:42 +0100

    sepolia and holesky

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
 const RPC_ENDPOINTS = {
     'etherscan.io': "https://ethereum-rpc.publicnode.com",
+    'sepolia.etherscan.io': "https://ethereum-sepolia-rpc.publicnode.com",
+    'holesky.etherscan.io': "https://ethereum-holesky-rpc.publicnode.com",
     'polygonscan.com': "https://polygon-bor-rpc.publicnode.com"
 };
