ac4ff2062a0a0898ae110f4942a47a99e690baf2
tiagofneto <tiagofneto29@gmail.com>
2024-06-03 10:12:33 +0100

    bscscan, ftmscan, opscan and testnets

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,12 @@ const RPC_ENDPOINTS = {
     'etherscan.io': "https://ethereum-rpc.publicnode.com",
     'sepolia.etherscan.io': "https://ethereum-sepolia-rpc.publicnode.com",
     'holesky.etherscan.io': "https://ethereum-holesky-rpc.publicnode.com",
-    'polygonscan.com': "https://polygon-bor-rpc.publicnode.com"
+    'polygonscan.com': "https://polygon-bor-rpc.publicnode.com",
+    'amoy.polygonscan.com': "https://polygon-amoy-bor-rpc.publicnode.com",
+    'bscscan.com': "https://bsc-rpc.publicnode.com",
+    'testnet.bscscan.com': "https://bsc-testnet-rpc.publicnode.com",
+    'ftmscan.com': "https://fantom-rpc.publicnode.com",
+    'testnet.ftmscan.com': "https://fantom-testnet-rpc.publicnode.com",
+    'optimistic.etherscan.io': "https://optimism-rpc.publicnode.com",
+    'sepolia-optimism.etherscan.io': "https://optimism-sepolia-rpc.publicnode.com"
 };
